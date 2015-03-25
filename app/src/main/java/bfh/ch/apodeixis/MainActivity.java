301314935572d2710abf6882b3243891377d5825
bizkitishere </G1thub!t>
2015-03-25 17:47:54 +0100

    added some libraries

--- app/src/main/java/bfh/ch/apodeixis/MainActivity.java
+++ app/src/main/java/bfh/ch/apodeixis/MainActivity.java
@@ -1,17 +1,17 @@
 package bfh.ch.apodeixis;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
