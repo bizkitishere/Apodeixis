b42c1e354766b0994a76fd6b33525b4d7645d983
bizkitishere </G1thub!t>
2015-05-14 20:28:23 +0200

    sounds mqtt connection mqtt publish mqtt subscribe play sounds minimal gui

--- app/src/main/java/bfh/ch/apodeixis/MainActivity.java
+++ app/src/main/java/bfh/ch/apodeixis/MainActivity.java
@@ -2,16 +2,25 @@ package bfh.ch.apodeixis;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
+import android.location.GpsStatus;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
@@ -22,6 +31,122 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String broker = "tcp://broker.mqttdashboard.com";
+        String port = "1883";
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true);
+
+        final Context c = this.getApplicationContext();
+
+        final MqttAndroidClient client = new MqttAndroidClient(MainActivity.this, broker, "123");
+
+        //callback, for when a message arrives
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String s, final MqttMessage mqttMessage) throws Exception {
+                //make toast
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(c, "Message arrived: " + mqttMessage.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cough1);
+                mediaPlayer.start();
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+
+        //listens to the successfull connection to the broker
+        IMqttActionListener listener = new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken iMqttToken) {
+                //make toast
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(c, "Connected successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                try {
+                    client.subscribe("App/test", 2, null, null);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                //make toast
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(c, "Could not connect", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+
+
+
+
+
+        try {
+            client.connect(options, null, listener);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        final Button button = (Button) findViewById(R.id.btn_connect);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //read user data
+
+                /*
+                Intent datBundle = new Intent();
+
+                EditText etBroker = (EditText) findViewById(R.id.et_broker);
+                EditText etPort = (EditText) findViewById(R.id.et_port);
+
+                String broker = etBroker.getText().toString();
+                String port = etPort.getText().toString();
+
+                datBundle.putExtra((String.valueOf(R.id.et_broker)), broker);
+                datBundle.putExtra((String.valueOf(R.id.et_port)), port);
+                */
+
+
+                try {
+                    client.publish("App/test", "message".getBytes(), 1, false);
+                    //send toast
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(c, "Published message", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+
 
     }
 
